9d39c3f0be184263832d13026a088a1e9497039c
JiayiZhou36 <143651921+JiayiZhou36@users.noreply.github.com>
2023-09-24 14:08:23 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,19 @@
 """
 Test Cases
 """
-from main import func
+from main import os_and_sys_version
 
 
-def test_answer():
-    """test function"""
-    assert func(3) == 4
-    assert func(2) == 3
+def test_os_and_sys_version():
+    """Test os_and_sys_version"""
+    python_version, os_name = os_and_sys_version()
+    python_version_check = ["3.7", "3.8", "3.9", "3.10", "3.11"]
+    os_name_check = ["Windows", "Linux"]
+    assert python_version in python_version_check
+    # if environment has an OS installed
+    if os_name is not None:
+        assert os_name in os_name_check
+
+
+if __name__ == "__main__":
+    test_os_and_sys_version()
