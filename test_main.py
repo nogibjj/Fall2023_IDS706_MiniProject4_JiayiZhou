90db4b09cc9c5591230fcb631990cb3a38f6890f
JiayiZhou36 <143651921+JiayiZhou36@users.noreply.github.com>
2023-09-24 14:11:07 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ from main import os_and_sys_version
 def test_os_and_sys_version():
     """Test os_and_sys_version"""
     python_version, os_name = os_and_sys_version()
-    python_version_check = ["3.7", "3.8", "3.9", "3.10", "3.11"]
+    python_version_check = ["3.7", "3.8", "3.9", "3.11"]
     os_name_check = ["Windows", "Linux"]
     assert python_version in python_version_check
     # if environment has an OS installed
